60c528e210543cdaff505263a6193ea5edf5850d
Harish Madas <40024784+harishmadas501@users.noreply.github.com>
2018-07-26 18:51:50 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,5 @@
 $("#jsVolumeBall").mousemove(function(){
     var height = $("#jsVolumeLevel").height();
 	$("#jsVolumeLevel").css('height', height+1);
+	$("#jsVolumeLevel").css('width', '0px');
 });

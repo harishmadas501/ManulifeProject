55dde6dfe323684cdf0a53e08189b3983a3cf27b
Harish Madas <40024784+harishmadas501@users.noreply.github.com>
2018-07-26 15:09:23 +0530

    Update landing-page.js

--- js/landing-page.js
+++ js/landing-page.js
@@ -1998,7 +1998,7 @@ var VolumeSlider = {
         var percent = Math.ceil(value * 100);
         percent = percent > 100 ? 100 : percent;
 
-        $("#jsVolumeLevel").innerWidth(percent + "%");
+        $("#jsVolumeLevel").innerHeight(percent + "%");
         $("#jsVolumeButtonSRText").text("Volume (" + percent + "%)");
 
         try {

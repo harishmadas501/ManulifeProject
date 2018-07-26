e37ffb8fd23a7af3899fe388d6ac8ebdd1f46712
Harish Madas <40024784+harishmadas501@users.noreply.github.com>
2018-07-26 19:03:12 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,12 +2,12 @@ $("#jsVolumeBall").mousemove(function(){
     var height = $("#jsVolumeLevel").height();
 	if(height!=100)
 		$("#jsVolumeLevel").css('height', height+1);
-	$("#jsVolumeLevel").css('width', '0px');
+	$("#jsVolumeLevel").css('max-width', '0px');
 });
 
 $("#jsVolumeBall").mousedown(function(){
     var height = $("#jsVolumeLevel").height();
 	if(height!=0)
 		$("#jsVolumeLevel").css('height', height-1);
-	$("#jsVolumeLevel").css('width', '0px');
+	$("#jsVolumeLevel").css('max-width', '0px');
 });
